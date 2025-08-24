0bbadf06ccc7de89e8a8cacde5ff08925da341dc
sunit shakya <sunit07shakya@gmail.com>
2025-08-24 20:08:05 +0530

    filter applied

--- marketplace/views/product_views.py
+++ marketplace/views/product_views.py
@@ -1,11 +1,13 @@
 # your_app/views/product_views.py
-from rest_framework import generics
+from rest_framework import generics, filters
 from marketplace.models.product import Product,ProductCategory
 from marketplace.serializers.product_serializer import ProductSerializer,ProductCategorySerializer
 
-class ProductListView(generics.ListCreateAPIView):
-    queryset = Product.objects.all()
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.all().prefetch_related("inventories__company", "category")
     serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name", "category__name"]
 
 class CategoryListView(generics.ListCreateAPIView):
     queryset = ProductCategory.objects.all()
